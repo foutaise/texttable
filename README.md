d2ad4a2e0fa0cadff5c90cc646625d6cf83e725c
Gerome Fournier <jef@foutaise.org>
2019-02-15 11:21:10 +0100

    Add LICENSE, tests in source tarball

--- README.md
+++ README.md
@@ -218,10 +218,10 @@ DATA
     __author__ = 'Gerome Fournier <jef(at)foutaise.org>'
     __credits__ = 'Jeff Kowalczyk:\n    - textwrap improved import\n ...at...
     __license__ = 'MIT'
-    __version__ = '1.6.0'
+    __version__ = '1.6.1'
 
 VERSION
-    1.6.0
+    1.6.1
 
 AUTHOR
     Gerome Fournier <jef(at)foutaise.org>
--- setup.py
+++ setup.py
@@ -12,11 +12,11 @@ with open("README.md") as f:
 
 setup(
     name="texttable",
-    version="1.6.0",
+    version="1.6.1",
     author="Gerome Fournier",
     author_email="jef@foutaise.org",
     url="https://github.com/foutaise/texttable/",
-    download_url="https://github.com/foutaise/texttable/archive/v1.6.0.tar.gz",
+    download_url="https://github.com/foutaise/texttable/archive/v1.6.1.tar.gz",
     license="MIT",
     py_modules=["texttable"],
     description=DESCRIPTION,
